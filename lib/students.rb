0721c45266c08523ce0807a4cb9b704ff6743576
Peggy Griffin <seishingithub@gmail.com>
2014-06-28 15:42:39 -0600

    Initial commit passing spec 'can return all students'

---
+++ lib/students.rb
@@ -0,0 +1,11 @@
+class Students
+  def all_students
+    [
+      {name: "Peggy", birth_year: 1968},
+      {name: "Fred", birth_year: 1963},
+      {name: "Sarah", birth_year: 1981},
+      {name: "Lisa", birth_year: 1961},
+      {name: "Ian", birth_year: 1979},
+    ]
+  end
+end
---
+++ spec/students_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+require './lib/students'
+
+describe Students do
+  it 'can return all students' do
+    students = Students.new
+
+    expected_students = [
+      {name: "Peggy", birth_year: 1968},
+      {name: "Fred", birth_year: 1963},
+      {name: "Sarah", birth_year: 1981},
+      {name: "Lisa", birth_year: 1961},
+      {name: "Ian", birth_year: 1979},
+    ]
+
+    expect(students.all_students).to match_array expected_students
+  end
+
+  it 'can return names of all students' do
+    pending
+    students = Students.new
+
+    expected_names = [
+      "Peggy",
+      "Fred",
+      "Sarah",
+      "lisa",
+      "Ian"
+    ]
+
+    expect(students.student_names).to match_array expected_names
+
+  end
+
+  it 'can return students born before a certain year' do
+    pending
+    students = Students.new
+    expected_students = [
+      {name: "Peggy", birth_year: 1968},
+      {name: "Fred", birth_year: 1963},
+      {name: "Lisa", birth_year: 1961},
+    ]
+
+    expect(students.born_before_1970).to match_array expected_students
+  end
+
+  it 'can return students born after a certain year' do
+    pending
+    students = Students.new
+
+    expected_students = [
+      {name: "Sarah", birth_year: 1981},
+      {name: "Ian", birth_year: 1979},
+    ]
+
+    expect(students.born_after_1970).to match_array expected_students
+  end
+
+  it 'can return the names of students born after a certain year' do
+    pending
+    students = Students.new
+
+    expected_names = [
+      "Sarah",
+      "Ian",
+    ]
+
+    expect(students.student_names_born_after_1970).to match_array expected_names
+  end
+end
